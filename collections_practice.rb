3f551e68e2317176108fbbcbcdfa08540ac33504
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-18 21:55:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,5 +39,5 @@ def sum_array(array)
 end 
 
 def add_s(array) 
-  array.each_with_index.map{|element, index| element + "s" if index != 1}
+  array.each_with_index.map{|element, index| if index != 1 element + "s" else element}
 end 
